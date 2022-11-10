37116095dab548e7acd8305298f6351f8403e360
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-11-10 16:31:13 +0300

    Fix eslint errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Layout from './components/Layout'
-import { BrowserRouter as Router } from 'react-router-dom'
-import './styles/reset.scss'
+import { BrowserRouter as Router } from 'react-router-dom';
+import Layout from './components/Layout';
+import './styles/reset.scss';
 
 function App() {
   return (
--- src/pages/NoMatch.js
+++ src/pages/NoMatch.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 export default function NoMatch() {
   return (
     <div>NoMatch</div>
-  )
+  );
 }
