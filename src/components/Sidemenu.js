161b4b2e7de5c55204e0b3c5935d3efd2f4fa376
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-11-10 16:10:57 +0300

    Add all page links to sidemenu

--- src/components/Sidemenu.js
+++ src/components/Sidemenu.js
@@ -1,26 +1,40 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import logo from '../images/logo.png'
-import { FaHome } from 'react-icons/fa'
+import { FaCartPlus, FaChartArea, FaHome, FaLanguage, FaVideo } from 'react-icons/fa'
 import './Sidemenu.scss'
 
 export default function Sidemenu() {
   return (
     <nav className="sidemenu">
-        <NavLink to="/">
+        <NavLink to="/" className="sidemenu__logo">
             <img src={logo} alt="logo" />
         </NavLink>
         <ul>
             <li>
-                <NavLink to="/tutors" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>
-                    <FaHome /><span> Tutors</span>
+                <NavLink to="/tutors" className={({ isActive }) => (isActive ? 'active' : '')}>
+                    <FaHome /><span>Tutors</span>
                 </NavLink>
             </li>
             <li>
-                <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Languages</NavLink>
+                <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
+                    <FaLanguage /><span>Languages</span>
+                </NavLink>
             </li>
             <li>
-                <NavLink to="/lessons" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>My Lessons</NavLink>
+                <NavLink to="/lessons" className={({ isActive }) => (isActive ? 'active' : '')}>
+                    <FaVideo /><span>My Lessons</span>
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/statistics" className={({ isActive }) => (isActive ? 'active' : '')}>
+                    <FaChartArea /><span>Statistics</span>
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to="/shoppingcart" className={({ isActive }) => (isActive ? 'active' : '')}>
+                    <FaCartPlus /><span>Shopping Cart</span>
+                </NavLink>
             </li>
         </ul>
     </nav>
