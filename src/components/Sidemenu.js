6f2212fcda246ea36b95c3eb5aca8d01d6523fad
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-11-10 15:42:57 +0300

    Add padding to menu links

--- src/components/Sidemenu.js
+++ src/components/Sidemenu.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import logo from '../images/logo.png'
+import { FaHome } from 'react-icons/fa'
 import './Sidemenu.scss'
 
 export default function Sidemenu() {
@@ -11,15 +12,15 @@ export default function Sidemenu() {
         </NavLink>
         <ul>
             <li>
-                <NavLink to="/tutors">
-                    Tutors
+                <NavLink to="/tutors" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>
+                    <FaHome /><span> Tutors</span>
                 </NavLink>
             </li>
             <li>
-                <NavLink to="/">Languages</NavLink>
+                <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>Languages</NavLink>
             </li>
             <li>
-                <NavLink to="/lessons">My Lessons</NavLink>
+                <NavLink to="/lessons" className={({ isActive }) => (isActive ? 'active' : 'inactive')}>My Lessons</NavLink>
             </li>
         </ul>
     </nav>
