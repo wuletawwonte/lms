456b093b16d4c86855a9cd90b18f7e2d00a35042
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-11-11 17:48:20 +0300

    Add country change handler

--- src/components/FlagsDropdown.js
+++ src/components/FlagsDropdown.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import images from '../services/importFlags';
+import allCountries from '../services/countries';
 import './FlagsDropdown.scss';
 
 export default function FlagsDropdown() {
+  const [isActive, setIsActive] = useState(false);
+  const [country, setCountry] = useState('India.gif');
+
+  function handleClick() {
+    setIsActive(!isActive);
+  }
+
+  function changeCountry(countryName) {
+    setCountry(countryName);
+    setIsActive(false);
+  }
+
   return (
     <div className="flags">
-      <button type="button" className="flags__button">
+      <button type="button" className="flags__button" onClick={handleClick}>
         <img
-          src={images['Flag of Afghanistan.gif']}
+          src={images[country]}
           alt="Flags of Afghanistan"
           className="flags__button__image"
         />
       </button>
+      {isActive && (
+        <div className="flags__list">
+          {allCountries.map((item) => (
+            <button
+              type="button"
+              key={item}
+              onClick={() => changeCountry(item)}
+            >
+              {item}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
