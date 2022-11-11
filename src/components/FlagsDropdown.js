b826a7311eac06156fcb69f979f9b7ed81461084
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-11-11 15:43:19 +0300

    Add flags of countries of the world

---
+++ src/components/FlagsDropdown.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './FlagsDropdown.scss';
+
+export default function FlagsDropdown() {
+  return <div>FlagsDropdown</div>;
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import FlagsDropdown from './FlagsDropdown';
 import './Header.scss';
 
 export default function Header() {
@@ -7,6 +8,7 @@ export default function Header() {
     <header className="header">
       <span>M</span>
       <Link to="/">Welcome</Link>
+      <FlagsDropdown />
     </header>
   );
 }
