9779c38c716c3e43efe87de52fa3738e6a0490c0
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-11-13 15:50:06 +0300

    Style search form in the languages page

--- src/pages/Languages.js
+++ src/pages/Languages.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+import './Languages.scss';
 
 export default function Languages() {
   return (
-    <div>Languages</div>
+    <div className="languages">
+      <form>
+        <input type="search" placeholder="Search Language" />
+        <button type="submit">
+          <FaSearch />
+        </button>
+      </form>
+    </div>
   );
 }
